0be2b4009107bc40620e72ce12e545ed1849973c
Adam Kosun <kosunadam@gmail.com>
2017-01-26 13:00:03 +0100

    some minor changes, comments

--- newsfeed.py
+++ newsfeed.py
@@ -3,12 +3,15 @@ import os
 
 feed = fp.parse('http://www.tvn24.pl/najnowsze.xml')
 
-rows, columns = os.popen('stty size', 'r').read().split()
+#Trying to be fancy and getting that terminal window width.
+_, columns = os.popen('stty size', 'r').read().split()
 
 for index, entry in enumerate(feed.entries):
     entry_title = entry.title.rstrip()
+    #The .split(...) removes one html tag they might want to smuggle there
     entry_description = str(entry.description).lstrip().split('/>', 1)[-1]
     entry_description = entry_description.lstrip().split(' - ',1)[-1].rstrip()
+
     print(str(index+1)+ "\n")
     print(entry_title + "\n" + "."*len(entry_title) + "\n\n" + entry_description)
     print("\n\n" + entry.link)
